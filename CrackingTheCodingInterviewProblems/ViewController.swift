74f536f1288a95a940f1ff5834ba0796abbcf442
lenli <leonard.li@gmail.com>
2016-08-18 09:58:26 -0400

    Initial Commit

---
+++ CrackingTheCodingInterviewProblems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrackingTheCodingInterviewProblems
+//
+//  Created by Leonard Li on 8/18/16.
+//  Copyright © 2016 Objective Li Labs LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
